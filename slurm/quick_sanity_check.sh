1ff511d414c09ea7337cc320fdfc6e1a85324c2c
Maksim Eremenko <Maksim.Eremenko@ruhr-uni-bochum.de>
2026-04-07 16:53:12 +0200

    slurm script fix

--- slurm/quick_sanity_check.sh
+++ slurm/quick_sanity_check.sh
@@ -2,6 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=fat_gpu
 #SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=1
 #SBATCH --job-name=sherlock_quick_sanity
 #SBATCH --account=melloldm_0000
 #SBATCH --time 1:00:00
